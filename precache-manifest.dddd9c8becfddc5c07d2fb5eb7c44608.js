6bf5dcb4fe449b7036fa7248db0045ae95c8ae62
marcosicp <marcosicp2012@gmail.com>
2021-06-14 06:06:28 -0300

    Updates

---
+++ precache-manifest.dddd9c8becfddc5c07d2fb5eb7c44608.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2a90318eda39eb359efe48078fba58c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a432b105a873975ed5d",
+    "url": "/static/css/2.6190f2dc.chunk.css"
+  },
+  {
+    "revision": "1bbb4c62be7e9056deea",
+    "url": "/static/css/main.56a1b1df.chunk.css"
+  },
+  {
+    "revision": "6a432b105a873975ed5d",
+    "url": "/static/js/2.e2ac82bb.chunk.js"
+  },
+  {
+    "revision": "898cc9f58085051141b5a96c9624f451",
+    "url": "/static/js/2.e2ac82bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bbb4c62be7e9056deea",
+    "url": "/static/js/main.437e652d.chunk.js"
+  },
+  {
+    "revision": "7712caea7ea1b0a4c9f0",
+    "url": "/static/js/runtime-main.59e7fa72.js"
+  },
+  {
+    "revision": "bc035cc0364d417b2855918a0e1d5b22",
+    "url": "/static/media/LOGO-AZUL-SIN-FONDO.bc035cc0.png"
+  },
+  {
+    "revision": "9282a89e5f62da8eb3580d618fa4f5a7",
+    "url": "/static/media/LOGO-BLANCO-SIN-FONDO.9282a89e.png"
+  },
+  {
+    "revision": "683f1cdb45353fc887e47f6823079a22",
+    "url": "/static/media/fondo1.683f1cdb.png"
+  },
+  {
+    "revision": "197e60c7b2dbd969ff29b0d31ab232f7",
+    "url": "/static/media/fondo2.197e60c7.png"
+  },
+  {
+    "revision": "371da91ee885d962946cfa6038832ba2",
+    "url": "/static/media/fondo3.371da91e.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/static/media/ionicons.621bd386.svg"
+  }
+]);
